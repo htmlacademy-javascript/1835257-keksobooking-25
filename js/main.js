1f1148d1b58b4263b62e2cc5952dd50380412bbe
PopovRY <rupopov91@gmail.com>
2022-03-31 14:12:54 +1000

    Добавил перевод в активное\неактивное состояние.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 import './markup.js';
 import './form-valid.js';
+import {getActiveState, getDisactiveState} from './form.js';
+import {advertisements} from './data.js';
+import {makeCardList} from './markup.js';
+
+getDisactiveState();
+
+setTimeout(getActiveState, 2000);
+
+document.querySelector('#map-canvas').appendChild(makeCardList(advertisements[0]));
